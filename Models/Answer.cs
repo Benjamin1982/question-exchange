b059420ab4d947cd9296501b3212995d3ce803ae
Benjamin1982 <benjaminz.1@gmx.de>
2021-06-18 14:15:58 +0300

    Adding empty controller class to projrct 16

---
+++ Models/Answer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace questionexchange.Models
+{
+    // The Reference to the Question
+    public class Answer
+    {
+        public bool wasFound { get; set; }
+        public static int _index = 0;
+        public static int _indexCorrect = 0; // How many correct answers?
+        public string content { get; set; }
+        public int index { get; set; }
+        public Answer() //schema-test: Answer a = new Answer(); Answer b = new Answer(); Console.WriteLine(a._index); Console.WriteLine(b._index);
+        {
+            wasFound = false;
+            _index++;
+        }
+    }
+}
