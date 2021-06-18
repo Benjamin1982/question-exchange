f4ae292e8bb73adcd491455e8cfa6f0ba56dbc41
Benjamin1982 <benjaminz.1@gmx.de>
2021-06-18 14:06:25 +0300

    Testing async string success

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,9 @@
 
 
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http; // Jetzt Müsste es gehen!
+using Microsoft.Extensions.DependencyInjection;
 
 namespace questionexchange
 {
@@ -9,22 +12,20 @@ namespace questionexchange
     
         public void ConfigureServices(IServiceCollection saas)
         {
-                // The cycle repeated ...
-
+            // Add a sprinkling of MVC
+            //For MVC to spring into life, you need to make some changes to startup.cs.
+            saas.AddMvc();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment  paas)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment paas)
         {
-            if (paas.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseMvc();
             // Waiting for updates -...
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("THE CYCLE REPEATED.. ");
             });
-    
-}
+        }
+    } // Klammer hat gefehlt
 
 }
